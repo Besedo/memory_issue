4561a3cff9731bfcf1990de94599e0e17f1c43bb
Syrine CHERIAA <syrine.cheriaa@gmail.com>
2023-08-22 14:17:10 +0200

    add sanic

---
+++ test_sanic/main.py
@@ -0,0 +1,14 @@
+from sanic import Sanic, response
+import asyncio
+app = Sanic("MyHelloWorldApp")
+
+# @app.post("/infer")
+# async def infer(request):
+#     data = request.json
+#     return response.json(len(data))
+
+@app.post("/infer")
+async def infer(request):
+    data = request.json
+    await asyncio.sleep(0.1)
+    return response.json("done")
